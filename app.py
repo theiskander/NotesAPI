1c4e4020114c506fbd585db2515d3a86f8684c3e
theiskander <iskander.burlai@gmail.com>
2025-02-19 12:00:52 +0200

    Added DB configuration

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from extensions import db
 
 def create_app():
     app = Flask(__name__)
     
     # Configuration
     app.config['SECRET_KEY'] = 's6q3e4f5gfjeh901i2j3k4l5mrn0ohdc'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    # Initialize extensions
+    db.init_app(app)
     
     @app.route('/')
     def index():
