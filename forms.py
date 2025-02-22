f5897cfa698af54489c7b065d495e07693c6cd3a
theiskander <iskander.burlai@gmail.com>
2025-02-22 17:49:22 +0200

    Added the Registration form

---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError, Email
+
+from models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=64)])
+    email =  EmailField('Email', validators=[DataRequired(), Email(), Length(min=10, max=64)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('This name is already taken. Please choose another one')
