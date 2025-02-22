53282a269ddcaf60012c031e5214def85c0786ed
theiskander <iskander.burlai@gmail.com>
2025-02-22 17:41:31 +0200

    Added the User model

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from extensions import db
 from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -8,5 +9,20 @@ class Note(db.Model):
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), nullable = False)
     updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate = lambda: datetime.now(timezone.utc), nullable = False)
     
+    # Foreign Keys
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    
     def __repr__(self):
         return f"<Note {self.id}: {self.title}>"
+    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(32), unique = True, nullable = False)
+    email = db.Column(db.String(64), unique = True, nullable = False)
+    password_hash = db.Column(db.String(128), nullable = False)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
