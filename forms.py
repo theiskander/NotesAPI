2c9a894afa3298bfdc5eda114586e997ce979cad
theiskander <iskander.burlai@gmail.com>
2025-02-23 10:56:29 +0200

    Added email validation to the Registration form

--- forms.py
+++ forms.py
@@ -15,3 +15,8 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('This name is already taken. Please choose another one')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('This name is already taken. Please choose another one')
